1ed4fef7b954c53764650805bd43bad840a40d87
Jordan Van Duyne <jordannn808@gmail.com>
2016-09-10 18:32:50 -0400

    Turning in my assignment for day 3

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -1,16 +1,36 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Jordan Van Duyne
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [x] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
 ;;; 1.  Create a calculator that takes one argument: a list that represents an expression.
 
 (define (calculate x)
-  your-code-here)
+  (cond [(eq? (car x) 'ADD) (+ (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'SUB) (- (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'MUL) (* (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'DIV) (/ (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'GT)  (> (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'LT)  (< (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'GE)  (>= (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'LE)  (<= (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'EQ)  (= (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+  		[(eq? (car x) 'NEQ) (not (= (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x))))))]
+  		[(eq? (car x) 'AND) (and (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+		[(eq? (car x) 'OR)  (or (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))))]
+		[(eq? (car x) 'NOT) (not (evaluate(car (cdr x))))]
+  		[(eq? (car x) 'IPH) (if (evaluate(car (cdr x))) (evaluate(car (cdr (cdr x)))) (evaluate(car (cdr (cdr (cdr x))))))]
+  		[(eq? (car x) 'UMINUS) (* (evaluate(car (cdr x))) -1)]))
+
+
+(define (evaluate x)
+  (cond [(number? x) x]
+  		[(boolean? x) x]
+  		[else (calculate x)]))
 
 (calculate '(ADD 3 4)) ;; --> 7
 
@@ -25,11 +45,21 @@
 
 	(calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
 	(calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
+;;	(calculate '(NEQ 2 3)) ;; --> #t
+;;	(calculate '(NEQ 3 3)) ;; --> #f
+;;	(calculate '(EQ 3 3)) ;;  --> #t
+;;	(calculate '(EQ 2 3)) ;;  --> #f
+	
 
 ;;; 4. Add boolean operations ANND, ORR, NOTT
 
-(calculate '(AND (GT (ADD 3 4) (MUL 5 6))) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
+(calculate '(AND (GT (ADD 3 4) (MUL 5 6)) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
 
 ;;; 5. Add IPH
 
 (calculate '(IPH (GT (ADD 3 4) 7) (ADD 1 2) (ADD 1 3))) ;; -> 4
+
+;;; 6. Add UMINUS
+;		given a number input, UMINUS will negate that number
+(calculate '(UMINUS 100)) ;; -> -100
+(calculate '(UMINUS -3)) ;; --> 3
