d58964ba7833f869493cca1d0d1361609a4968b0
Jordan Van Duyne <jordannn808@gmail.com>
2016-10-29 21:39:13 -0400

    turning in hw17

--- day17/graph.py
+++ day17/graph.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from Queue import Queue
 
 
 class Graph(object):
@@ -18,10 +18,12 @@ class Graph(object):
         return [head for head in self.nodes if node in self.successors(head)]
 
     def add_node(self, node):
-        raise NotImplementedError()
+        self.nodes.append(node)
+        self.adjacency_list[node] = [] 
 
     def add_edge(self, edge):
-        raise NotImplementedError()
+        self.adjacency_list[edge[0]].append(edge[1])
+        self.adjacency_list[edge[1]].append(edge[0])
 
 
 def bfs(graph, start):
@@ -40,5 +42,48 @@ def bfs(graph, start):
         n = remaining_nodes.get()
         visit(n)
 
+def create_spanning_tree(graph, start):
+    remaining_nodes = Queue()
+    visited = set()
+    spanning_tree = Graph([start], [])
+
+    def visit(node):
+        visited.add(node)
+        
+        for tail in graph.successors(node):
+            if tail not in visited:
+                remaining_nodes.put(tail)
+                spanning_tree.add_node(tail)
+                spanning_tree.add_edge((node, tail))
+
+    remaining_nodes.put(start)
+    while not remaining_nodes.empty():
+        n = remaining_nodes.get()
+        visit(n)
+
+    return spanning_tree
+
+def find_distances(graph, start):
+    remaining_nodes = Queue()
+    visited = set()
+
+    distances = {start: 0}
+
+    def visit(node):
+        visited.add(node)
+        for tail in graph.successors(node):
+            if tail not in visited:
+                remaining_nodes.put(tail)
+                distances[tail] = distances[node] + 1 # NOTE: this isn't necessarily the SHORTEST distance
+
+    remaining_nodes.put(start)
+    while not remaining_nodes.empty():
+        n = remaining_nodes.get()
+        visit(n)
+
+    return distances
+
 g = Graph(['a', 'b', 'c', 'd', 'e'], [('a', 'b'), ('a', 'c'), ('b', 'd'), ('b', 'e'), ('e', 'a')])
 bfs(g, 'a')
+print create_spanning_tree(g, 'a').adjacency_list
+print find_distances(g, 'a')
